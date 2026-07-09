7563b5789c2e56d3765f4810715086361c5bd5cf
Sulaiman Sulaiman <SKSulai98@gmail.com>
2026-07-10 00:01:28 +0100

    Added: Power management class stub

---
+++ src/PowerState.cs
@@ -0,0 +1,23 @@
+
+namespace PcMQTT
+{
+    
+    class PowerManager
+    {
+        
+        void hibernate()
+        {
+            // Code to put the computer into hibernate mode
+        }
+        void sleep()
+        {
+            // Code to put the computer into sleep mode
+        }
+        void shutdown()
+        {
+            // Code to shut down the computer
+        }
+
+    }
+
+}
